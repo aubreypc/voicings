3949c9b5de297eae3a015f718c67cdcf9d1fdd3c
Aubrey Collins <aubreypcollins@gmail.com>
2018-08-20 11:13:40 -0400

    Fixed QueryResult comparisons

--- voicings/voicings.py
+++ voicings/voicings.py
@@ -1,10 +1,12 @@
-from .fingerings import Fingering
 from collections import Counter
+from .fingerings import Fingering
 
 class QueryResult:
     def __init__(self, fingering, query_notes):
         self.rank = fingering.rank(notes=query_notes)
-        self.complete = not any(fingering.note_counts[n] == 0 for n in query_notes)
+        self.fretted = fingering.fretted
+        note_count = fingering.note_count(notes=query_notes)
+        self.complete = not any(note_count[n] == 0 for n in query_notes)
 
     def __lt__(self, other):
         return (self.complete, self.rank) < (other.complete, other.rank)
