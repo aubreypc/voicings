5f688e8003418c9a0ec3bff3465e9e15c80659de
Aubrey Collins <aubreypcollins@gmail.com>
2018-08-21 05:37:58 -0400

    New voicings() method to parse chord name, generate voicings, and rank the results

--- voicings/voicings.py
+++ voicings/voicings.py
@@ -1,10 +1,11 @@
 from collections import Counter
-from .fingerings import Fingering
+from .fingerings import FingeringsGenerator
+from .music import parse_chord_name
 
 class QueryResult:
     def __init__(self, fingering, query_notes):
         self.rank = fingering.rank(notes=query_notes)
-        self.fretted = fingering.fretted
+        self.fretted = fingering.fretted.copy()
         note_count = fingering.note_count(notes=query_notes)
         self.complete = not any(note_count[n] == 0 for n in query_notes)
 
@@ -25,3 +26,18 @@ class QueryResult:
 
     def __ne__(self, other):
         return (self.complete, self.rank) != (other.complete, other.rank)
+
+
+def voicings(chord_str):
+    """
+    Fetch all of a chord's voicings and yield them in sorted order.
+    """
+    gen = FingeringsGenerator()
+    chord = parse_chord_name(chord_str)
+    res = []
+    for voicing in gen.generate(chord):
+        prev = voicing
+        q = QueryResult(voicing, chord)
+        res.append(q)
+    res.sort()
+    return res
